ba93741d5fabddd67ada12d80886d2383c52627e
黄小亮 <chenglianghuang@aliyun.com>
2019-12-05 23:43:42 +0800

    登陆日志列表与删除完成 2019年12月5日 23:43:13

--- src/main/java/com/hcl/warehouse/sys/controller/SysLoginfoController.java
+++ src/main/java/com/hcl/warehouse/sys/controller/SysLoginfoController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-
 /**
  * <p>
  *  前端控制器
